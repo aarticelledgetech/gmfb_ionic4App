80f167b1a39c9fe6981dd087c1a8472be4201f60
aarticelledgetech <aarticelledgetech@gmail.com>
2019-05-02 14:03:59 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+
+import { Platform } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html'
+})
+export class AppComponent {
+  public appMenu = [
+    {
+      title: 'Downloadable',
+      url: '/list',
+      icon: 'list'
+    },
+    {
+      title: 'Play Online',
+      url: '/list',
+      icon: 'list'
+    }
+  ];
+
+
+  constructor(
+    private platform: Platform,
+    private splashScreen: SplashScreen,
+    private statusBar: StatusBar
+  ) {
+    this.initializeApp();
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
+    });
+  }
+}
